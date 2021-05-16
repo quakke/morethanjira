a72bc781287d4586f26ebceca01275996f5a28b7
quak <>
2021-05-16 16:36:45 +0300

    deleted unusing files

--- MoreThanJira/App.cs
+++ MoreThanJira/App.cs
@@ -1,18 +1,14 @@
 ﻿using System;
+using System.IO;
+using MoreThanJira.Data;
 
 namespace MoreThanJira
 {
     public class App
     {
-        public static bool UseMockDataStore = true;
-        public static string BackendUrl = "http://localhost:5000";
-
         public static void Initialize()
         {
-            if (UseMockDataStore)
-                ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
-            else
-                ServiceLocator.Instance.Register<IDataStore<Item>, CloudDataStore>();
+
         }
     }
 }
